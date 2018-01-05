4d61c9d99a4b58e0568ea67bfad79b0d6bc325b4
flaviogf <flavio.fernandes6@etec.sp.gov.br>
2018-01-05 11:34:35 -0200

    module

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-declare type State = any;
+declare module 'redux-flow' {
+  declare type State = any;
 
-declare type GetState = () => State;
+  declare type GetState = () => State;
 
-declare type Action = {
-  type: string,
-  payload: any
-};
+  declare type Action = {
+    type: string,
+    payload: any
+  };
 
-declare type PromiseAction = Promise<Action>;
+  declare type PromiseAction = Promise<Action>;
 
-declare type ThunkAction = (dispatch: Dispatch, getState: GetState) => any;
+  declare type ThunkAction = (dispatch: Dispatch, getState: GetState) => any;
 
-declare type Dispatch = (
-  action: Action | ThunkAction | PromiseAction | Array<Action>
-) => any;
+  declare type Dispatch = (
+    action: Action | ThunkAction | PromiseAction | Array<Action>
+  ) => any;
+}
